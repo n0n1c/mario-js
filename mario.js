11b6fbe6ab1c3b656637dd61aefdfb69f5f12b87
Nong Suon <nong.suon@gmail.com>
2017-03-23 16:36:53 -0400

    added error checking to mario.js

--- mario.js
+++ mario.js
@@ -20,7 +20,10 @@ formElem.onsubmit = function(event) {
     // TODO 1
     // if they didn't type anything at all, give a different error message,
     // something like "Please provide a height"
-
+    if (heightStr == "") {
+      displayError("Please provide a height.");
+      return;
+    }
 
     // convert the string to an int
     height = parseInt(heightStr);
@@ -28,7 +31,7 @@ formElem.onsubmit = function(event) {
     // if the height is not-a-number, yell at them and exit early
     // TODO 2
     // negative numbers and zero should also be rejected here
-    if (isNaN(height)) {
+    if (isNaN(height) || height < 1) {
         displayError("That's not a valid height.");
         return;
     }
